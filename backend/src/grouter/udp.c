66d4ed2a53c0c989f7c256fbcf3a60ec1c2e03af
Alex A <alexandre.allaire@mail.mcgill.ca>
2011-10-21 21:44:54 -0400

    final version checksum disabled

--- backend/src/grouter/udp.c
+++ backend/src/grouter/udp.c
@@ -33,9 +33,7 @@ uint16_t udp_cksum(ip_packet_t *iphdr, udphdr_t *hdr, uint8_t *data)
 	buf[8] = 0x0;
 	buf[9] = UDP_PROTOCOL;
 	buf[10] = htons(ntohs(iphdr->ip_pkt_len) - iphdr->ip_hdr_len * 4); 
-
-	printf("%d\n", hdr->len);
-
+	
 	int data_len = ntohs(hdr->len) - UDP_HEADER_SIZE;
 
 	memcpy(buf + PHEADER_SIZE, hdr, UDP_HEADER_SIZE);
@@ -45,47 +43,28 @@ uint16_t udp_cksum(ip_packet_t *iphdr, udphdr_t *hdr, uint8_t *data)
 		data_len++;		                                                                                  
 	}
 	
-	if (checksum((uchar *) buf, (PHEADER_SIZE + UDP_HEADER_SIZE + data_len) / 2) != mychecksum(buf, PHEADER_SIZE + UDP_HEADER_SIZE + data_len)) {
-		printf("puzzle.\n");
-	}
-
-	char str[DEFAULT_MTU] = {0};
-	memcpy(str, buf + PHEADER_SIZE + UDP_HEADER_SIZE, data_len);
-	printf("data_len -> %d\n", data_len);
-	printf("data -> %s", str);
-	printf("%d\n", (PHEADER_SIZE + UDP_HEADER_SIZE + data_len) / 2);	
-	
-	return checksum((uchar *)buf, (PHEADER_SIZE + UDP_HEADER_SIZE + data_len) / 2);
-	//return mychecksum(buf, PHEADER_SIZE + UDP_HEADER_SIZE + data_len);
+	//return checksum((uchar *)buf, (PHEADER_SIZE + UDP_HEADER_SIZE + data_len) / 2);
+	return mychecksum(buf, PHEADER_SIZE + UDP_HEADER_SIZE + data_len);
 }
 
-uint16_t mychecksum(uint8_t *data, int len)
+uint16_t mychecksum(uint8_t *buf, int len)
 {
-	uint32_t sum;
-	uint16_t word;
-	
+	uint32_t sum = 0;
 
 	while (len > 1) {
-
-		memcpy(&word, data, 2);
-
-		printf("%x %x ", word & 0xff, word & 0xff00);	
-
-		sum += word;
-		
-		data += 2;
+		sum += * (uint16_t *) buf++;
 		len -= 2;
 	}
 
 	if (len > 0) {
-		sum += *data;
+		sum += * (uint8_t *) buf;
 	}
 
 	while (sum >> 16) {
 		sum = (sum & 0xffff) + (sum >> 16);
 	}
 
-	return ~sum;
+	return (uint16_t) ~sum;
 }
 
 // RAPPEL caller les fonctions pour convertir en network byte order ou l'inverse
@@ -115,26 +94,23 @@ int send_udp(uint8_t dest_ip[4], uint16_t dest_port, uint16_t src_port, char *da
 	hdr->check = 0;
 	hdr->len = htons(len + UDP_HEADER_SIZE);
 
-	printf("gpkt ip udp : %x %x %x\n", out_pkt, ipkt, hdr);
-
 	memcpy((uchar *) hdr+UDP_HEADER_SIZE, data, len);
 	
 	// copies src and dest addresses into IP header in newtork byte order
 	getsrcaddr(out_pkt,dest_ip);
 	
-	char tmp[4] = {0};
+	uchar tmp[4] = {0};
 	COPY_IP(ipkt->ip_dst, gHtonl(tmp, dest_ip));
 
-	printf("u -> %x\n", hdr);
-
+	/*
 	// calculate checkum with everything in network byte order	
 	hdr->check = htons(udp_cksum(ipkt, hdr, (uint8_t *) data));
 
 	if(hdr->check == 0){
 		hdr->check = ~hdr->check;
 	}
+	*/
 
-	//hdr->check = 0;
 	// send
 	IPOutgoingPacket(out_pkt, dest_ip, ntohs(hdr->len), 1, UDP_PROTOCOL);
 
@@ -153,43 +129,26 @@ void udp_recv(gpacket_t *packet)
     // convert header to right byte order 
     udphdr_t *udpHeader = (udphdr_t *)((uint8_t *)ipPacket + ipPacket->ip_hdr_len*4);
 
-    /*
-    udpHeader->source = ntohs(udpHeader->source);
-    udpHeader->dest = ntohs(udpHeader->dest);
-    udpHeader->len = ntohs(udpHeader->len);
-    udpHeader->check = ntohs(udpHeader->check);    
-    */
-
-    uint8_t *data;
-    data = (uint8_t *) udpHeader + UDP_HEADER_SIZE;
+    uint8_t *data = (uint8_t *) udpHeader + UDP_HEADER_SIZE;
     int dataLength = ntohs(udpHeader->len) - UDP_HEADER_SIZE;
  
-/*    
+    /*    
     if (udpHeader->check !=0) 
     {
-        udpChecksum = udpHeader->check;
-	
-	printf("packet sum - > %u\n", udpChecksum);
-
-        udpHeader->check = 0;
-        tempChecksum = cksum(ipPacket, udpHeader, data);
-
-	printf("computed sum -> %u\n", tempChecksum);
-
-        if (udpChecksum != tempChecksum) 
-        {
-	    verbose(1, "[udp_recv]:: jai domper un packet");
-            return;
-        }
+	// dump packet
+       if (udp_cksum(ipPacket, udpHeader, data) != 0) {
+		return;
+	} 
     }
-*/
-	// verifie que le port est rouvert
+    */
+
+    // verifie que le port est rouvert
     if (port_open(ntohs(udpHeader->dest), UDP_PROTOCOL) == 0) 
     {
         ICMPProcessPortUnreachable(packet);
         return;
     }
    
-	// donne le data au port layer (write_data(port, proto, buf len)) 
-	write_data(ntohs(udpHeader->dest),UDP_PROTOCOL,data,dataLength);
+     // donne le data au port layer (write_data(port, proto, buf len)) 
+     write_data(ntohs(udpHeader->dest),UDP_PROTOCOL,data,dataLength);
 }
